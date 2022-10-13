e0709f4fec052fb2f98854e01786177fd37bf81d
aiexgt <lima14florian14@gmail.com>
2022-10-13 09:19:10 -0600

    Aplicación con animaciones y funcional

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
 import { VerPaisComponent } from '../pages/ver-pais/ver-pais.component';
 
@@ -12,19 +12,24 @@ export class PaisService {
   //https://restcountries.com/v3.1/name/united
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
+  get httpParams (){
+    return new HttpParams()
+      .set( 'fields', 'flags,capital,name,population,cca2' )
+  }
+
   constructor( private http: HttpClient ) { }
 
   buscarPais( termino: string ): Observable<Country[]> {
 
-    const url = `${this.apiUrl}/name/${termino}`;
-    return this.http.get<Country[]>( url );
+    const url = `${this.apiUrl}/name/${termino}?fields=flags,capital,name,population,cca2,ccn3,translations`;
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
 
   }
 
   buscarCapital( termino: string ): Observable<Country[]> {
 
-    const url = `${this.apiUrl}/capital/${termino}`;
-    return this.http.get<Country[]>( url );
+    const url = `${this.apiUrl}/capital/${termino}?fields=flags,capital,name,population,cca2,ccn3,translations`;
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
 
   }
 
@@ -34,5 +39,12 @@ export class PaisService {
     return this.http.get<Country>( url );
 
   }
+
+  buscarRegion( region: string): Observable<Country[]>{
+
+    const url = `${this.apiUrl}/region/${region}`;
+    return this.http.get<Country[]>( url, { params: this.httpParams } )
+    
+  } 
 
 }
